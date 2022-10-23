085870e558c7a854774fa0548f4e70a9bf65555b
maksimb <maksim.barabanov2002@mail.ru>
2022-10-23 17:43:07 +0500

    lesson8

---
+++ taks2.py
@@ -0,0 +1,63 @@
+"""
+Задание 2.
+Доработайте пример структуры "дерево", рассмотренный на уроке.
+Предложите варианты доработки и оптимизации
+(например, валидация значений узлов в соответствии
+ с требованиями для бинарного дерева). При валидации приветствуется генерация
+ собственного исключения
+Поработайте с оптимизированной структурой,
+протестируйте на реальных данных - на клиентском коде
+"""
+class BinaryTree:
+    def __init__(self, root_obj):
+        self.root = root_obj
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left(self, new_node):
+        if self.right_child is None or new_node < int(self.right_child.get_root_val()):
+            if self.left_child is None:
+                self.left_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.left_child = self.left_child
+                self.left_child = tree_obj
+        else:
+            raise ValueError('Значение левого потомка должно быть меньше, чем у правого потомка')
+
+    def insert_right(self, new_node):
+        if self.left_child is None or new_node > int(self.left_child.get_root_val()):
+            if self.right_child is None:
+                self.right_child = BinaryTree(new_node)
+            else:
+                tree_obj = BinaryTree(new_node)
+                tree_obj.right_child = self.right_child
+                self.right_child = tree_obj
+        else:
+            raise ValueError('Значение правого потомка должно быть больше, чем у левого потомка')
+
+    def get_right_child(self):
+        return self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root_val(self, obj):
+        self.root = obj
+
+    def get_root_val(self):
+        return self.root
+
+
+r = BinaryTree(8)
+print(r.get_root_val())
+print(r.get_left_child())
+r.insert_left(40)
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+r.insert_right(12)
+# r.insert_right(45)  # Корректный вариант
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+r.get_right_child().set_root_val(16)
+print(r.get_right_child().get_root_val())
